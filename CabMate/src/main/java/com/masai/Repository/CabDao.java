190609b0070af710bdeb957bdd3c77faaa21dfe5
Ashish Gupta <askd442@gmail.com>
2022-12-14 21:37:23 +0530

    added fw20_0771_day-2

--- CabMate/src/main/java/com/masai/Repository/CabDao.java
+++
@@ -1,6 +0,0 @@
-package com.masai.Repository;
-
-
-public interface CabDao{
-
-}
--- CabMate/src/main/java/com/masai/Repository/DriverDao.java
+++ CabMate/src/main/java/com/masai/Repository/DriverDao.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Driver extends JpaRepository<Driver, Integer>{
+public interface DriverDao extends JpaRepository<Driver, Integer>{
 
 }
