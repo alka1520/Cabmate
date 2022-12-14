a5852f5f5bc47ed58ad6e79952aa170d66b3cfb8
Mayank Gupta <107461052+mynkgupta22@users.noreply.github.com>
2022-12-14 18:05:56 +0530

    Delete -interesting-debt-3332 directory

--- -interesting-debt-3332/CabMate/src/main/java/com/masai/Repository/UserSessionDao.java
+++
@@ -1,9 +0,0 @@
-package com.masai.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.masai.Entities.UserSession;
-
-public interface UserSessionDao extends JpaRepository<UserSession, Integer>{
-
-}
